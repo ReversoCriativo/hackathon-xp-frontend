4a24fd26625b4596cfb2aaba71df4ec108a2d49d
erickgato <itsbk0703@gmail.com>
2022-02-20 21:02:51 -0300

    feat: add global bank toggle tab

--- src/components/organisms/UserBankList/index.tsx
+++ src/components/organisms/UserBankList/index.tsx
@@ -5,9 +5,10 @@ import BankCard from '../../atoms/BankCard'
 
 interface Props {
   user: UserProps
+  isVisible: boolean;
 }
 
-export default function UserBankList({ user }: Props) {
+export default function UserBankList({ user, isVisible }: Props) {
   return (
     <Flex gap={5} alignItems={'center'} justifyContent='center' width={'100%'} >
       {user.banks.map(({ checking: { balance }, institution: { bankName } }, index) => (
@@ -15,6 +16,7 @@ export default function UserBankList({ user }: Props) {
           key={Math.random() + index}
           bankName={bankName}
           value={balance}
+          on={isVisible}
         />
       ))}
     </Flex>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,21 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Button, Text } from '@chakra-ui/react'
 
 import { Navbar } from '../components/molecules/Navbar'
 import { useUser } from '../hooks/useUser'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useDashboard } from '../hooks/useDashboard'
 import UserBankList from '../components/organisms/UserBankList'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
 export default function Dashboard() {
   const { isLoading } = useUser()
   const { navbarSlugActive } = useDashboard()
   const { user } = useUser()
+  const [lastTabActive, setLastTabActive] = useState(false)
 
   const GetDashoboard = useCallback(() => {
     if (navbarSlugActive === 'my-balances') {
-      return <UserBankList user={user} />
+      return <UserBankList user={user} isVisible={lastTabActive} />
     }
     if (navbarSlugActive === 'my-investments') {
       return <Text>Meus Investimentos</Text>
@@ -21,7 +23,39 @@ export default function Dashboard() {
     if (navbarSlugActive === 'invest-now') {
       return <Text>Investir agora</Text>
     }
-  }, [user, navbarSlugActive])
+  }, [user, navbarSlugActive, lastTabActive])
+
+  const getNavbarLastTab = useCallback(() => {
+    if (navbarSlugActive === 'my-balances') {
+      return (
+        <>
+          <Button
+            variant='ghost'
+            _hover={{
+              opacity: 0.7,
+            }}
+            onClick={() => setLastTabActive(!lastTabActive)}
+          >
+            {lastTabActive ? (
+              <FiEyeOff fontSize={22} style={{ marginRight: '15px' }} />
+            ) : (
+              <FiEye fontSize={22} style={{ marginRight: '15px' }} />
+            )}
+            {lastTabActive
+              ? 'Esconder todos os saldos'
+              : 'Mostrar todos os saldos'}
+          </Button>
+        </>
+      )
+    }
+
+    if (navbarSlugActive === 'my-investments') {
+      return <></>
+    }
+    if (navbarSlugActive === 'invest-now') {
+      return <></>
+    }
+  }, [navbarSlugActive, lastTabActive])
 
   return (
     <Box
@@ -31,7 +65,7 @@ export default function Dashboard() {
       alignItems='center'
       justifyContent='center'
     >
-      <Navbar />
+      <Navbar leftTabComponent={getNavbarLastTab()} />
       {!isLoading ? GetDashoboard() : null}
     </Box>
   )
